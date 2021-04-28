c7e0e579ccd1be3b2d33bc9280eaaab81437a050
Paul Le Mercier <paul.lemercier05@gmail.com>
2021-04-28 15:42:54 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const header = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+	
+	burger.addEventListener('click', () => {
+		//Toggle Nav
+		header.classList.toggle('nav-active');
+
+
+		//Animate Links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+			link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+			}
+		});
+		// Burger Animation 
+		burger.classList.toggle('toggle');
+
+	});
+
+
+}
+
+navSlide();
+
